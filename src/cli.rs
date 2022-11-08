b26f65505662cd7c118b42b2ed428c5d7095789f
SoraSuegami <suegamisora@gmail.com>
2022-11-08 23:17:50 +0900

    Fix the error handling in the cli.rs.

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,15 @@
-use anyhow::Result;
-use once_cell::sync::Lazy;
-use promkit::{build::Builder, readline};
+//use anyhow::Result;
+use promkit::{build::Builder, crossterm::style, readline};
 use sommelier_drive_client::*;
-use std::cell::RefCell;
-use std::io;
+use std::{cell::RefCell, process::exit};
 use tokio::runtime::Runtime;
 
-fn main() -> Result<()> {
+fn main() -> Result<(), String> {
     #[cfg(not(feature = "cli"))]
     panic!("To run the cli, you need to compile this crate with `cli` feature.");
     #[cfg(feature = "cli")]
     let region_name = "sommelier_drive_cli";
-    let rt = Runtime::new()?;
+    let rt = Runtime::new().expect("Runtime init error");
     let fut_result = async {
         cli(region_name).await.unwrap();
     };
@@ -19,104 +17,151 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-async fn cli(region_name: &'static str) -> Result<()> {
-    let mut p = readline::Builder::default().build()?;
-    let base_url = p.run()?;
+async fn cli(region_name: &'static str) -> Result<(), String> {
+    let mut p = readline::Builder::default()
+        .title("Enter your server url")
+        .title_color(style::Color::DarkGreen)
+        .build()
+        .expect("readline init error");
+    let base_url = p.run().expect("Server url not found.");
+    println!("Welcome to Sommelier Drive!");
+    let mut p = readline::Builder::default()
+        .build()
+        .expect("readline init error");
+    loop {
+        let line = p.run().expect("Fail to get next prompt.");
+        let result = perform_each_op(&base_url, region_name, line).await;
+        match result {
+            Err(e) => println!("{}", e.to_string()),
+            _ => {}
+        }
+    }
+}
+
+async fn perform_each_op(
+    base_url: &str,
+    region_name: &'static str,
+    line: String,
+) -> Result<(), String> {
     thread_local! {
         pub static GLOBAL_USER_INFO: RefCell<Option<SelfUserInfo>> = RefCell::new(None)
     };
     let client = HttpClient::new(&base_url, region_name);
-    loop {
-        let line = p.run()?;
-        let mut spw = line.split_whitespace();
-        let command = spw.next().expect("Command not found.");
-        match command {
-            "register" => {
-                let filepath = spw.next().expect("Initial directory path not found.");
-                let new_info = register_user(&client, filepath).await?;
-                println!("You are registered with a user id {}", new_info.id);
-                GLOBAL_USER_INFO.with(|info| {
-                    let mut info_ref = info.borrow_mut();
-                    *info_ref = Some(new_info);
-                });
-            }
-            "touch" => {
-                let filepath = spw.next().expect("File path not found.");
-                let user_info = GLOBAL_USER_INFO
-                    .with(|info| info.borrow().clone().expect("You have to login first."));
-                add_file(&client, &user_info, filepath, Vec::new()).await?
-            }
-            "mkdir" => {
-                let filepath = spw.next().expect("File path not found.");
-                let user_info = GLOBAL_USER_INFO
-                    .with(|info| info.borrow().clone().expect("You have to login first."));
-                add_directory(&client, &user_info, filepath).await?
-            }
-            "cat" => {
-                let filepath = spw.next().expect("File path not found.");
-                let user_info = GLOBAL_USER_INFO
-                    .with(|info| info.borrow().clone().expect("You have to login first."));
-                let is_exist = is_exist_filepath(&client, &user_info, filepath).await?;
-                if !is_exist {
-                    panic!("The given filepath {} does not exist.", filepath);
-                }
-                let contents_data = open_filepath(&client, &user_info, filepath).await?;
-                if !contents_data.is_file {
-                    panic!(
-                        "The file at filepath {} is not a file but a directory.",
-                        filepath
-                    );
-                }
-                println!("{}", String::from_utf8(contents_data.file_bytes)?);
-            }
-            "modify" => {
-                let filepath = spw.next().expect("File path not found.");
-                let text = spw.next().expect("Input text not found.");
-                let user_info = GLOBAL_USER_INFO
-                    .with(|info| info.borrow().clone().expect("You have to login first."));
-                let is_exist = is_exist_filepath(&client, &user_info, filepath).await?;
-                if !is_exist {
-                    panic!("The given filepath {} does not exist.", filepath);
-                }
-                let contents_data = open_filepath(&client, &user_info, filepath).await?;
-                if !contents_data.is_file {
-                    panic!(
-                        "The file at filepath {} is not a file but a directory.",
-                        filepath
-                    );
-                }
-                modify_file(&client, &user_info, filepath, text.as_bytes().to_vec()).await?;
-            }
-            "ls" => {
-                let filepath = spw.next().expect("File path not found.");
-                let user_info = GLOBAL_USER_INFO
-                    .with(|info| info.borrow().clone().expect("You have to login first."));
-                let is_exist = is_exist_filepath(&client, &user_info, filepath).await?;
-                if !is_exist {
-                    panic!("The given filepath {} does not exist.", filepath);
-                }
-                let children_pathes = get_children_pathes(&client, &user_info, filepath).await?;
-                for path in children_pathes {
-                    println!("{}", path);
-                }
-            }
-            "find" => {
-                let filepath = spw.next().expect("File path not found.");
-                let user_info = GLOBAL_USER_INFO
-                    .with(|info| info.borrow().clone().expect("You have to login first."));
-                let is_exist = is_exist_filepath(&client, &user_info, filepath).await?;
-                if !is_exist {
-                    panic!("The given filepath {} does not exist.", filepath);
-                }
-                let pathes = search_descendant_pathes(&client, &user_info, filepath).await?;
-                for path in pathes {
-                    println!("{}", path);
-                }
-            }
-            "exit" => {
-                return Ok(());
-            }
-            _ => panic!("{} is not supported", command),
+    let mut spw = line.split_whitespace();
+    let command = spw.next().expect("Command not found.");
+    match command {
+        "register" => {
+            let filepath = spw.next().ok_or("Initial directory path not found.")?;
+            let new_info = register_user(&client, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            println!("You are registered with a user id {}", new_info.id);
+            GLOBAL_USER_INFO.with(|info| {
+                let mut info_ref = info.borrow_mut();
+                *info_ref = Some(new_info);
+            });
         }
+        "touch" => {
+            let filepath = spw.next().ok_or("File path not found.")?;
+            let user_info = GLOBAL_USER_INFO
+                .with(|info| info.borrow().clone().ok_or("You have to login first."))?;
+            add_file(&client, &user_info, filepath, Vec::new())
+                .await
+                .map_err(|e| e.to_string())?;
+        }
+        "mkdir" => {
+            let filepath = spw.next().ok_or("File path not found.")?;
+            let user_info = GLOBAL_USER_INFO
+                .with(|info| info.borrow().clone().ok_or("You have to login first."))?;
+            add_directory(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+        }
+        "cat" => {
+            let filepath = spw.next().ok_or("File path not found.")?;
+            let user_info = GLOBAL_USER_INFO
+                .with(|info| info.borrow().clone().ok_or("You have to login first."))?;
+            let is_exist = is_exist_filepath(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            if !is_exist {
+                return Err(format!("The given filepath {} does not exist.", filepath));
+            }
+            let contents_data = open_filepath(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            if !contents_data.is_file {
+                return Err(format!(
+                    "The file at filepath {} is not a file but a directory.",
+                    filepath
+                ));
+            }
+            let bytes_string =
+                String::from_utf8(contents_data.file_bytes).map_err(|e| e.to_string())?;
+            println!("{}", bytes_string);
+        }
+        "modify" => {
+            let filepath = spw.next().ok_or("File path not found.")?;
+            let text = spw.next().ok_or("Input text not found.")?;
+            let user_info = GLOBAL_USER_INFO
+                .with(|info| info.borrow().clone().ok_or("You have to login first."))?;
+            let is_exist = is_exist_filepath(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            if !is_exist {
+                return Err(format!("The given filepath {} does not exist.", filepath));
+            }
+            let contents_data = open_filepath(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            if !contents_data.is_file {
+                return Err(format!(
+                    "The file at filepath {} is not a file but a directory.",
+                    filepath
+                ));
+            }
+            modify_file(&client, &user_info, filepath, text.as_bytes().to_vec())
+                .await
+                .map_err(|e| e.to_string())?;
+        }
+        "ls" => {
+            let filepath = spw.next().ok_or("File path not found.")?;
+            let user_info = GLOBAL_USER_INFO
+                .with(|info| info.borrow().clone().ok_or("You have to login first."))?;
+            let is_exist = is_exist_filepath(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            if !is_exist {
+                return Err(format!("The given filepath {} does not exist.", filepath));
+            }
+            let children_pathes = get_children_pathes(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            for path in children_pathes {
+                println!("{}", path);
+            }
+        }
+        "find" => {
+            let filepath = spw.next().ok_or("File path not found.")?;
+            let user_info = GLOBAL_USER_INFO
+                .with(|info| info.borrow().clone().ok_or("You have to login first."))?;
+            let is_exist = is_exist_filepath(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            if !is_exist {
+                return Err(format!("The given filepath {} does not exist.", filepath));
+            }
+            let pathes = search_descendant_pathes(&client, &user_info, filepath)
+                .await
+                .map_err(|e| e.to_string())?;
+            for path in pathes {
+                println!("{}", path);
+            }
+        }
+        "exit" => {
+            exit(0);
+        }
+        _ => return Err(format!("{} is not supported", command)),
     }
+    Ok(())
 }
